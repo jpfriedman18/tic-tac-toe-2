ff7436cda7f9db99a5d9446d3fce2b797cd972be
jpfriedman18 <jpfriedman18@gmail.com>
2016-02-04 18:19:50 -0500

    Got board to render from back-end App is now acceptable to turn in

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,158 +13,6 @@ const myApp = {
   BASE_URL: 'http://tic-tac-toe.wdibos.com'
 };
 
-//AJAX requests
-$(document).ready(() => {
-  $('.signed-out').show();
-  $('.signed-in').hide();
-  //Create new user
-  $('#sign-up').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target);
-    $.ajax({
-      url: myApp.BASE_URL + '/sign-up',
-      method: 'POST',
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      console.log(data);
-      $('#sign-up-modal').modal('hide');
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
-  });
-
-  //Login as existing user
-  $('#sign-in').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target);
-    $.ajax({
-      url: myApp.BASE_URL + '/sign-in',
-      method: 'POST',
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      console.log(data);
-      myApp.user = data.user;
-      console.log(myApp.user);
-      $('.signed-out').hide();
-      $('.signed-in').show();
-      $('#sign-in-modal').modal('hide');
-      ajaxCreateGame();
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
-  });
-
-  //Change password of currently logged-in user
-  $('#change-password').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(e.target);
-    $.ajax({
-      url: myApp.BASE_URL + '/change-password/' + myApp.user.id,
-      method: 'PATCH',
-      headers: {
-        Authorization: 'Token token=' + myApp.user.token,
-      },
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      console.log(data);
-      $('#change-password-modal').modal('hide');
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
-  });
-
-  //Log out
-  $('#sign-out-button').on('click', function(e) {
-    e.preventDefault();
-    $.ajax({
-      url: myApp.BASE_URL + '/sign-out/' + myApp.user.id,
-      method: 'DELETE',
-      headers: {
-        Authorization: 'Token token=' + myApp.user.token,
-      },
-    }).done(function() {
-      console.log("Logged Out!");
-      $('.signed-out').show();
-      $('.signed-in').hide();
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
-  });
-});
-
-//Create new game instance on server with blank board
-//associated with currently logged-in email
-let ajaxCreateGame = function(){
-  $.ajax({
-    url: myApp.BASE_URL + '/games',
-    method: 'POST',
-    headers: {
-      Authorization: 'Token token=' + myApp.user.token,
-    },
-    data: {}
-  })
-  .done(function(data){
-    myApp.game = data.game;
-    console.log(myApp.game);
-    ajaxGetGame();
-  })
-  .fail(function(jqxhr) {
-    console.error(jqxhr);
-  });
-};
-
-//Updates back-end game instance with new move
-let ajaxUpdateGame = function(player, index){
-  $.ajax({
-    url: myApp.BASE_URL + '/games/' + myApp.game.id,
-    method: 'PATCH',
-    headers: {
-      Authorization: 'Token token=' + myApp.user.token,
-    },
-    data: {
-      "game": {
-        "cell": {
-          "index": index,
-          "value": player
-        }
-//    "over": false
-      }
-    }
-  })
-  .done(function(){
-    ajaxGetGame();
-  })
-  .fail(function(jqxhr) {
-    console.error(jqxhr);
-  });
-};
-
-//Retrieves current game state from back-end
-let ajaxGetGame = function(){
-  $.ajax({
-    url: myApp.BASE_URL + '/games/' + myApp.game.id,
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + myApp.user.token,
-    }
-  })
-  .done(function(data){
-    myApp.game = data.game;
-    console.log(myApp.game);
-    console.log(myApp.game.cells);
-  })
-  .fail(function(jqxhr) {
-    console.error(jqxhr);
-  });
-};
-
-let playGame = function(){
   let currentPlayer = 'X';
   let turnCounter = 0;
   let xWins = 0;
@@ -172,6 +20,25 @@ let playGame = function(){
 
   let changePlayer = function(){
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  };
+
+  //clear board, switch player (loser goes first), reset turn counter
+  let resetBoard = function(){
+    ajaxCreateGame();
+    $('#board').find('td').text('');
+    changePlayer();
+    turnCounter = 0;
+  };
+
+  let updateScoreboard = function(player){
+    if (player === 'X'){
+      xWins++;
+      $('#x-score').text('X Wins: ' + xWins);
+    }
+    else{
+      oWins++;
+      $('#o-score').text('O Wins: ' + oWins);
+    }
   };
 
   let checkWinner = function(player){
@@ -242,41 +109,174 @@ let playGame = function(){
   //play an individual turn
   $('#board').find('td').on('click', function(){
     if ($(this).text() === ''){
-      $(this).text(currentPlayer);
+      //$(this).text(currentPlayer);
       ajaxUpdateGame(currentPlayer, ($(this).attr('id')) - 1);
+    }
+  });
+
+
+  let renderBoard = function(gameArray){
+    gameArray.forEach(function(element, index){
+      let square = index + 1;
+      $('#' + square).text(element);
+    });
+  };
+
+  //AJAX requests
+
+  //Retrieves current game state from back-end
+  let ajaxGetGame = function(){
+    $.ajax({
+      url: myApp.BASE_URL + '/games/' + myApp.game.id,
+      method: 'GET',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      }
+    })
+    .done(function(data){
+      myApp.game = data.game;
+      renderBoard(myApp.game.cells);
+      console.log(myApp.game);
+      console.log(myApp.game.cells);
       checkWinner(currentPlayer);
       changePlayer();
       $('#currentPlayer').text('Current Player: ' + currentPlayer);
       turnCounter++;
-    }
-  });
-
-  let renderBoard = function(gameArray){
-    gameArray.forEach(function(element, index){
-      
+    })
+    .fail(function(jqxhr) {
+      console.error(jqxhr);
     });
   };
 
-  //clear board, switch player (loser goes first), reset turn counter
-  let resetBoard = function(){
-    ajaxCreateGame();
-    $('#board').find('td').text('');
-    changePlayer();
-    turnCounter = 0;
+  //Create new game instance on server with blank board
+  //associated with currently logged-in email
+  let ajaxCreateGame = function(){
+    $.ajax({
+      url: myApp.BASE_URL + '/games',
+      method: 'POST',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      data: {}
+    })
+    .done(function(data){
+      myApp.game = data.game;
+      console.log(myApp.game);
+      renderBoard(myApp.game.cells);
+      //ajaxGetGame();
+    })
+    .fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
   };
 
-  let updateScoreboard = function(player){
-    if (player === 'X'){
-      xWins++;
-      $('#x-score').text('X Wins: ' + xWins);
-    }
-    else{
-      oWins++;
-      $('#o-score').text('O Wins: ' + oWins);
-    }
+  //Updates back-end game instance with new move
+  let ajaxUpdateGame = function(player, index){
+    $.ajax({
+      url: myApp.BASE_URL + '/games/' + myApp.game.id,
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      data: {
+        "game": {
+          "cell": {
+            "index": index,
+            "value": player
+          }
+  //    "over": false
+        }
+      }
+    })
+    .done(function(){
+      ajaxGetGame();
+    })
+    .fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
   };
-};
 
-$(document).ready(() => {
-  playGame();
-});
+  //Account AJAX requests
+  $(document).ready(() => {
+    $('.signed-out').show();
+    $('.signed-in').hide();
+    //Create new user
+    $('#sign-up').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(e.target);
+      $.ajax({
+        url: myApp.BASE_URL + '/sign-up',
+        method: 'POST',
+        contentType: false,
+        processData: false,
+        data: formData,
+      }).done(function(data) {
+        console.log(data);
+        $('#sign-up-modal').modal('hide');
+      }).fail(function(jqxhr) {
+        console.error(jqxhr);
+      });
+    });
+
+    //Login as existing user
+    $('#sign-in').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(e.target);
+      $.ajax({
+        url: myApp.BASE_URL + '/sign-in',
+        method: 'POST',
+        contentType: false,
+        processData: false,
+        data: formData,
+      }).done(function(data) {
+        console.log(data);
+        myApp.user = data.user;
+        console.log(myApp.user);
+        $('.signed-out').hide();
+        $('.signed-in').show();
+        $('#sign-in-modal').modal('hide');
+        ajaxCreateGame();
+      }).fail(function(jqxhr) {
+        console.error(jqxhr);
+      });
+    });
+
+    //Change password of currently logged-in user
+    $('#change-password').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(e.target);
+      $.ajax({
+        url: myApp.BASE_URL + '/change-password/' + myApp.user.id,
+        method: 'PATCH',
+        headers: {
+          Authorization: 'Token token=' + myApp.user.token,
+        },
+        contentType: false,
+        processData: false,
+        data: formData,
+      }).done(function(data) {
+        console.log(data);
+        $('#change-password-modal').modal('hide');
+      }).fail(function(jqxhr) {
+        console.error(jqxhr);
+      });
+    });
+
+    //Log out
+    $('#sign-out-button').on('click', function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: myApp.BASE_URL + '/sign-out/' + myApp.user.id,
+        method: 'DELETE',
+        headers: {
+          Authorization: 'Token token=' + myApp.user.token,
+        },
+      }).done(function() {
+        console.log("Logged Out!");
+        $('.signed-out').show();
+        $('.signed-in').hide();
+      }).fail(function(jqxhr) {
+        console.error(jqxhr);
+      });
+    });
+  });
